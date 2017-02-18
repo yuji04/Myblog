4cb17a863d47248bb484920f88b61338a78ee028
Yuji Yamaaguchi <04yuji.yamaguchi@gmail.com>
2017-02-18 13:33:46 +0000

    Add point

---
+++ db/migrate/20170218132835_add_point_to_message.rb
@@ -0,0 +1,5 @@
+class AddPointToMessage < ActiveRecord::Migration[5.0]
+  def change
+    add_column :messages, :point, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218083137) do
+ActiveRecord::Schema.define(version: 20170218132835) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170218083137) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
+    t.integer  "point"
   end
 
 end
